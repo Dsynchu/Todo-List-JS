2dbdc32011c42e152e6238658857f66a1235557c
student dsync <chetrii007@gmail.com>
2025-07-02 00:39:58 +0530

    created css file

--- todo.js
+++ todo.js
@@ -22,3 +22,29 @@ taskInput.value="";
 
 } ;
 
+
+
+// function addTask(){
+//     let taskInput=document.getElementById("taskInput");
+//     let tasktext=taskInput.value.trim();
+
+
+//     if(tasktext===""){
+//         alert("please, Enter your task.")
+//         return;
+//     }
+
+
+//     let li=document.createElement("li");
+//     li.textContent=tasktask;
+
+//     let deletebtn=document.createElement("button");
+//     deletebtn.textContent="delete";
+//     button.onclick=function(){
+//         li.remove;
+//     };
+//     li.appendChild(button);
+//     document.getElementById("taskList").appendChild(li);
+//     taskInput.value="";
+
+// };
